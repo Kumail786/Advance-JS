f81615aa66fa12b1e26fd6f5595a83aa6053dfc1
Kumail786 <kumailrawjani900@gmail.com>
2020-01-09 00:52:15 +0500

    arrow functions

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("hi")
+//Before ES6
+
+function sayHello(name) {
+    console.log("hello " + name)
+}
+
+sayHello("Kumail")
+
+//OR
+
+var sayHello = function (name) {
+    console.log("hello " + name)
+}
+
+sayHello("Ali")
+
+//ES6 (arrow Function)
+
+var sayHello = (name) => {
+    console.log("Hello " + name)
+}
+
+sayHello("Raza")
